8fd76ae710815c5025b5b64e658ddcb90f3572d8
DmChernyh <mich6u4@gmail.com>
2022-10-12 22:32:21 +0300

    first

--- src/main/java/ru/kata/spring/boot_security/demo/init/InitConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/init/InitConfig.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.demo.config;
+package ru.kata.spring.boot_security.demo.init;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
